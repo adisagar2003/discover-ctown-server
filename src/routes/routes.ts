453d5e161ee67a50dff60db094a5d9b938067800
Adi <adityasagarpython@gmail.com>
2024-07-24 11:32:59 -0300

    Populated locations to the db, get route implement

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import userController from "../controllers/user.controller";
 import authController from "../controllers/auth.controller";
 import locationController from "../controllers/location.controller";
+import locationMapController from "../controllers/locationmap.controller";
 
-const api = Router().use(userController).use(authController).use(locationController);
+const api = Router().use(userController).use(authController).use(locationController).use(locationMapController);
 
 export default Router().use('/api', api);
