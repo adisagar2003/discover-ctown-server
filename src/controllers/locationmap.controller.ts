df5211fe8defd337bddede094d271ac5a59d3655
Adi <adityasagarpython@gmail.com>
2024-08-09 12:24:31 -0300

    Search functionality added for browse page

--- src/controllers/locationmap.controller.ts
+++ src/controllers/locationmap.controller.ts
@@ -14,4 +14,22 @@ router.get('/locationMap/:count', async (req, res) => {
     })
 })
 
+// get location in terms of search param
+router.get('/locationMap/search/:name', async (req, res) => {
+    
+    const locationMaps = await prisma.locationMap.findMany({where:{properties:{
+        path: ['name'], string_contains:req.params.name
+    }},
+    take: 10,
+    skip: 0
+});
+
+    if (!locationMaps) return res.status(200).json({error: 'No locations found'});
+
+    res.status(200).json({
+        response: locationMaps
+    })
+
+})
+
 export default router;
